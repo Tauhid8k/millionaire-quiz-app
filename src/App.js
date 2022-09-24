9a584f7e82924124859536150d3f70ae5f801ebc
Tauhid8k <tauhid8k@gmail.com>
2022-09-25 03:08:30 +0600

    basic pyramid money list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 function App() {
-  return <h1>React Millionaire Quiz App</h1>;
+  const moneyPyramid = [
+    { id: 1, amount: '$100' },
+    { id: 2, amount: '$200' },
+    { id: 3, amount: '$300' },
+    { id: 4, amount: '$500' },
+    { id: 5, amount: '$1000' },
+    { id: 6, amount: '$2000' },
+    { id: 7, amount: '$4000' },
+    { id: 8, amount: '$8000' },
+    { id: 9, amount: '$16000' },
+    { id: 10, amount: '$32000' },
+    { id: 11, amount: '$64000' },
+    { id: 12, amount: '$125000' },
+    { id: 13, amount: '$250000' },
+    { id: 14, amount: '$500000' },
+    { id: 15, amount: '$1000000' },
+  ];
+
+  return (
+    <div className="app">
+      <main>Main</main>
+      <div className="pyramid">
+        <ul className="money-list">
+          {moneyPyramid.map((m) => (
+            <li className="money-list-item" key={m.id}>
+              <span className="list-item-number">{m.id}</span>
+              <span className="list-item-amount">{m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
