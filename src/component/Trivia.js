05f286c670a83612bc0742bb805c61377309a28c
Tauhid8k <tauhid8k@gmail.com>
2022-09-26 19:10:05 +0600

    all basic design done

--- src/component/Trivia.js
+++ src/component/Trivia.js
@@ -1,14 +1,24 @@
 const Trivia = () => {
   return (
     <div className="trivia">
-      <div className="question">
-        What's the best youtube channel for coding?
-      </div>
-      <div className="answers">
-        <div className="answer wrong">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
+      <div
+        className="container"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          textAlign: 'center',
+        }}
+      >
+        <div className="question">
+          What's the best youtube channel for coding?
+        </div>
+        <div className="answers">
+          <div className="answer wrong">Lama Dev</div>
+          <div className="answer">Lama Dev</div>
+          <div className="answer">Lama Dev</div>
+          <div className="answer">Lama Dev</div>
+        </div>
       </div>
     </div>
   );
