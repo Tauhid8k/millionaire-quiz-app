d6ae4c69757bbca28e4eccd7491c452f37a39ca8
Tauhid8k <tauhid8k@gmail.com>
2022-09-27 23:28:58 +0600

    all question added

--- src/data.js
+++ src/data.js
@@ -109,6 +109,161 @@ const questionData = [
       },
     ],
   },
+  {
+    id: 6,
+    question: `'OS' computer abbreviation usually means ?`,
+    answers: [
+      {
+        text: 'Order of Significance',
+        correct: false,
+      },
+      {
+        text: 'Open Software',
+        correct: false,
+      },
+      {
+        text: 'Operating System',
+        correct: true,
+      },
+      {
+        text: 'Optical Sensor',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question:
+      'Which is a type of Electrically-Erasable Programmable Read-Only Memory?',
+    answers: [
+      {
+        text: 'Flash',
+        correct: true,
+      },
+      {
+        text: 'Flange',
+        correct: false,
+      },
+      {
+        text: 'Fury',
+        correct: false,
+      },
+      {
+        text: 'FRAM',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: `Who developed Yahoo?`,
+    answers: [
+      {
+        text: 'Dennis Ritchie & Ken Thompson',
+        correct: false,
+      },
+      {
+        text: 'David Filo & Jerry Yang',
+        correct: true,
+      },
+      {
+        text: 'Vint Cerf & Robert Kahn',
+        correct: false,
+      },
+      {
+        text: 'Steve Case & Jeff Bezos',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: `	What does VVVF stand for?`,
+    answers: [
+      {
+        text: 'Variant Voltage Vile Frequency',
+        correct: false,
+      },
+      {
+        text: 'Variable Velocity Variable Fun',
+        correct: false,
+      },
+      {
+        text: 'Very Very Vicious Frequency',
+        correct: false,
+      },
+      {
+        text: 'Variable Voltage Variable Frequency',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: `What frequency range is the High Frequency band?`,
+    answers: [
+      {
+        text: '100 kHz',
+        correct: false,
+      },
+      {
+        text: '1 GHz',
+        correct: false,
+      },
+      {
+        text: '30 to 300 MHz',
+        correct: false,
+      },
+      {
+        text: '3 to 30 MHz',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: `What is the relationship between resistivity r and conductivity s?`,
+    answers: [
+      {
+        text: 'R = s2',
+        correct: false,
+      },
+      {
+        text: 'R = s',
+        correct: false,
+      },
+      {
+        text: 'R > s',
+        correct: false,
+      },
+      {
+        text: 'R = 1/s',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: `What does the term PLC stand for?`,
+    answers: [
+      {
+        text: 'Programmable Lift Computer',
+        correct: false,
+      },
+      {
+        text: 'Program List Control',
+        correct: false,
+      },
+      {
+        text: 'Programmable Logic Controller',
+        correct: true,
+      },
+      {
+        text: 'Piezo Lamp Connector',
+        correct: false,
+      },
+    ],
+  },
 ];
 
 const moneyPyramid = [
